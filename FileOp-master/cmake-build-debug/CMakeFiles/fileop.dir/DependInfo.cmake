c73d8d6b8736746886a975b6e04ffee051534039
eminecalis <eminecalis@hotmail.de>
2016-12-11 19:34:39 +0100

    Add files via upload
    liste sortieren, liste in eine datei schreiben

---
+++ FileOp-master/cmake-build-debug/CMakeFiles/fileop.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/emine/ClionProjects/FileOp-master/list.c" "/home/emine/ClionProjects/FileOp-master/cmake-build-debug/CMakeFiles/fileop.dir/list.c.o"
+  "/home/emine/ClionProjects/FileOp-master/main.c" "/home/emine/ClionProjects/FileOp-master/cmake-build-debug/CMakeFiles/fileop.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/usr/include/glib-2.0"
+  "/usr/lib/x86_64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
