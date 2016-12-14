c9c58d56e5ecd232de10c9bb930c62fda17648b7
Sebastian Geiger <sbastig@gmx.net>
2016-12-14 13:38:35 +0100

    Aenderungen von letzter woche

--- glib-practice/main.c
+++ glib-practice/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <glib.h>
+#include <string.h>
+#include <unistd.h>
 
 #define LENGTH   80
 typedef struct contact {
@@ -9,26 +11,29 @@ typedef struct contact {
   char *telefonnumber;
 }Contact;
 
+gint sort_by_name (gconstpointer a, gconstpointer contact2);
+void print_table (GList *mylist);
+void write_contact (gpointer data, gpointer user_data);
 
+int main(int argc, char *argv[]) {
 
-int main() {
   Contact *contact;
   char buffer[LENGTH];
 
   GList *mylist = NULL;
-
+  int index = 0;
   while(1)
   {
-    int index = 0;
+
     contact = malloc (sizeof(Contact));
     mylist = g_list_append (mylist,contact);
     contact->index = index;
     printf("Name: \n");
     fgets(buffer,LENGTH,stdin);
-    contact->name = g_strdup (buffer);
+    contact->name = g_strndup (buffer,strlen(buffer)-1);
     printf("Telefonnumber: \n");
     fgets(buffer,LENGTH,stdin);
-    contact->telefonnumber = g_strdup (buffer);
+    contact->telefonnumber = g_strndup (buffer, strlen(buffer)-1);
     printf("Moechten Sie noch einen Contact eingeben? [J/n] \n");
     fgets(buffer,LENGTH,stdin);
     int morecontacts = (!g_strcmp0(buffer,"J\n")) || (!g_strcmp0 (buffer,"j\n")) || (!g_strcmp0 (buffer,"\n"));
@@ -38,14 +43,39 @@ int main() {
     }
     index++;
   }
-  while(mylist != NULL)
-  {
-    fgets(buffer,LENGTH,stdin);
-    mylist = g_list_sort(mylist,GCompareFunc(mylist,buffer));
 
-    mylist = mylist->next;
-  }
+    //fgets(buffer,LENGTH,stdin);
+    mylist = g_list_sort(mylist, sort_by_name);
+    FILE *file = fopen(argv[1],"w+");
+    g_list_foreach(mylist,write_contact,file);
+    fclose (file);
+
+    print_table (mylist);
 
 return (EXIT_SUCCESS);
 }
+void write_contact (gpointer data, gpointer user_data)
+{
+  char *name = "Hello\n";
+  FILE *file = user_data;
+  fwrite(name,strlen(name),1,file);
+}
+gint sort_by_name (gconstpointer a, gconstpointer b)
+{
+    Contact *contact1 = (Contact *) a;
+    Contact *contact2 = (Contact *) b;
+    return g_strcmp0 (contact1->name, contact2->name);
+}
+void print_contact(gpointer data, gpointer user_data)
+{
+  Contact *contact =data;
+  printf("%10i| %-20s|%-25s|\n",contact->index, contact->name, contact->telefonnumber);
+}
+void print_table (GList *mylist){
 
+  printf("================================================================\n");
+  printf("Index     | Name                |Tel                      |\n");
+  g_list_foreach(mylist,print_contact,NULL);
+  printf("================================================================\n");
+
+}
