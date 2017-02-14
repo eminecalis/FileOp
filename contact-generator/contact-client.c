9fd3f392d0e0154753724cfb1ec7b996378281c5
Sebastian Geiger <sbastig@gmx.net>
2017-02-14 16:38:51 +0100

    contact-client

---
+++ contact-generator/contact-client.c
@@ -0,0 +1,5 @@
+//
+// Created by sebastiangeiger on 14/02/17.
+//
+
+#include "contact-client.h"
---
+++ contact-generator/contact-client.h
@@ -0,0 +1,8 @@
+//
+// Created by sebastiangeiger on 14/02/17.
+//
+
+#ifndef FILEOP_CONTACT_CLIENT_H
+#define FILEOP_CONTACT_CLIENT_H
+
+#endif //FILEOP_CONTACT_CLIENT_H
