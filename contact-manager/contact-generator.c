7cb0f3167a891c911a66190516b3178cb5af64e9
Sebastian Geiger <sbastig@gmx.net>
2017-04-21 15:47:47 +0200

    Addressbook v2

--- contact-manager/contact-generator.c
+++ contact-manager/contact-generator.c
@@ -1,10 +1,15 @@
 //
 // Created by sebastiangeiger on 31/03/17.
 //
+#define _XOPEN_SOURCE 500
 
 #include "contact-generator.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "contact.h"
+#include <glib.h>
+
 
 
 int main (int argc, char *argv[])
@@ -15,9 +20,57 @@ int main (int argc, char *argv[])
    ungefaehr 100 kontakte mit einer
    Schleife
    speichere alle kontakte (als struct) in einer Liste
-   */
 
+   Todo: erweitere den Teil mit den Nachnamen!
+   */
+   Contact *contact;
+   char *names[] = {"Peter","Stephan","Martin","Paris","Marina"};
+   GList *mylist = NULL;
+
+   for (int i = 0; i <100; i++)
+   {
+
+     contact = malloc(sizeof(Contact));
+     int indexName = random() % G_N_ELEMENTS(names);
+     contact->name = names[indexName];
+     char *telNumber;
+     telNumber = malloc(4*sizeof(char));
+     for(int j =0; j<4;j++)
+     {
+      snprintf (&telNumber[j],1,"%d",(int)(random()%10));
+     }
+     contact->telefonnumber = telNumber;
+     contact->index =i+1;
+
+     g_list_append (mylist, contact);
+   }
   //wait for request
+
+   char buffer[1024];
+   char* bytesRead = fgets (buffer,1024,stdin);
+   if(bytesRead == NULL)
+   {
+     printf("Error reading input\n");
+     return EXIT_FAILURE;
+   }
+   size_t bufferLength = strlen(buffer);
+   if ( buffer[bufferLength] != '\n')
+   {
+     // wird das naechste Mal behandelt
+     return EXIT_FAILURE;
+   }
+   /* Parent schickt den
+    *
+    * <command>: "exit", "search_name", "search_number", "search_index"
+    * <data>: "..."
+    *
+    * "COMMAND: <command>\n"
+    * "<data>\n"
+    * ""
+    *
+    */
+
+
 
   /*search contact
    * search funktion von der
---
+++ contact-manager/contact.h
@@ -0,0 +1,13 @@
+//
+// Created by sebastiangeiger on 21/04/17.
+//
+
+#ifndef FILEOP_CONTACT_H
+#define FILEOP_CONTACT_H
+
+typedef struct contact {
+  int index;
+  char *name;
+  char *telefonnumber;
+}Contact;
+#endif //FILEOP_CONTACT_H
