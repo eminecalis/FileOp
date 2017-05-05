22ee89892ebe7641080c0d5e2734c81634676232
Sebastian Geiger <sbastig@gmx.net>
2017-05-05 16:21:24 +0200

    contact-generator-client

--- contact-manager/contact-client.c
+++ contact-manager/contact-client.c
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <glib.h>
+#include <string.h>
+#include <wait.h>
 
 #define PIPE_READ 0
 #define PIPE_WRITE 1
@@ -31,7 +34,7 @@ int main (int argc, char *argv[])
   dup2(read_channel[PIPE_WRITE],STDOUT_FILENO);
   close(read_channel[PIPE_WRITE]);
   char *args[] = {"/home/sebastiangeiger/ClionProjects/FileOp/cmake-build-debug/contact-manager-generator", NULL};
-  int exec_result = execve ("/home/sebastiangeiger/ClionProjects/FileOp/cmake-build-debug/contact-manager-generator", args,NULL);
+  int exec_result = execve (args[0], args, NULL);
   if (exec_result == -1)
   {
    printf("Generator failed to start child process!\n");
@@ -40,20 +43,47 @@ int main (int argc, char *argv[])
  }
  else
  {
-  //ask user --> typ, query
-  // zuhause
+   close(write_channel[PIPE_READ]);
+   close(read_channel[PIPE_WRITE]);
+   //ask user --> typ, query
+   // zuhause
 
-  enum Type {NAME, TEL};
+   enum Type {NAME, TEL};
 
-  enum Type type = NAME;
-  char *query = "Max";
+   enum Type type = NAME;
+   char *command = "search_name";
 
-  // send to generator
+   // send to generator
 
+   //Command: <command>\n
+   //<data>\n
+   //\n
 
-  //wait for reply
+   char *data = "Peter";
+   gchar *buffer = g_strdup_printf ("Command: %s\n%s\n",command,data);
+   printf("%s\n",buffer);
+   ssize_t write_elements = write(write_channel[PIPE_WRITE],buffer,strlen(buffer));
+   //printf("%d\n",write_elements);
+   //printf("%d\n",strlen(buffer));
+   g_free (buffer);
 
-  //print
+   char readbuffer[1024] = {};
+   read(read_channel[PIPE_READ],readbuffer,1024);
+   //readbuffer[strstr (readbuffer, "\n")];
+   printf("%s\n",readbuffer);
+
+   //ask child to exit
+   buffer = g_strdup_printf ("Command: exit\n");
+   //printf("%s\n",buffer);
+   write(write_channel[PIPE_WRITE],buffer,strlen(buffer));
+   g_free (buffer);
+
+   //wait for reply
+   int status;
+   wait(&status);
+   //waitpid(pid, &status, 0);
+   printf ("Child process endet mit: %d\n", WEXITSTATUS (status));
+   //print
  }
 
 
